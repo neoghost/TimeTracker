f38d9bf95172f80af7aeffaad6435a5775211733
Alon Kerem <Alon.Kerem@live.com>
2016-12-28 01:00:29 +0200

    latest

--- AKTimeTracker/MainWindow.xaml.cs
+++ AKTimeTracker/MainWindow.xaml.cs
@@ -1,8 +1,16 @@
 ﻿
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,10 +32,16 @@ namespace AKTimeTracker
         public MainWindow()
         {
             InitializeComponent();
+            static string[] Scopes = { CalendarService.Scope.Calendar };
+            static string ApplicationName = "Google Calendar API .NET Quickstart";
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            // Initialize Calendar service with valid OAuth credentials
+            Google.Apis.Calendar.v3.Data.Calendar service = new Google.Apis.Calendar.v3.Data.Calendar();
+            service.Summary = textBoxDesc.ToString();
 
         }
     }
