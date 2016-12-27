9096e9fdc037a562c979f350d7740024f6514245
Alon Kerem <Alon.Kerem@live.com>
2016-12-27 20:50:42 +0200

    starting google init

--- AKTimeTracker/MainWindow.xaml.cs
+++ AKTimeTracker/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace AKTimeTracker
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
